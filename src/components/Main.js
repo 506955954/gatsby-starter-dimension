bc6f3b00e719639a20d0495c3bb2afec586c798a
Szymon Rakowski <duch_rak@interia.pl>
2020-02-12 16:35:35 +0100

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -117,7 +117,7 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Izraelski Szlak Narodowy</h2>
+          <h2 className="major">Izrl Szlak Narodowy</h2>
           <span className="image main">
             <img src={pic02} alt="" />
           </span>
