4468cf1d986e78dfcb5894b5b3d085bbd0f35929
Szymon Rakowski <duch_rak@interia.pl>
2020-11-18 20:25:02 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = props => (
       <div className="inner">
         <h1>Szymon Rakowski</h1>
         <p>
-          TRAVELS{' '}
+          Travels / Podróże{' '}
           <a href=""></a> 
           <br />
           {' '}
